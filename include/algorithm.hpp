b52ddecb88dc105eaf271937920f1529f1e9d772
Refi Go <64242999+refigo@users.noreply.github.com>
2023-02-15 16:21:03 +0900

    Implement equal() with __equal_to()

--- include/algorithm.hpp
+++ include/algorithm.hpp
@@ -3,8 +3,36 @@
 
 namespace ft {
 
+// __equal_to
 
-// less
+template <class _T1, class _T2 = _T1>
+struct __equal_to
+{
+    bool operator()(const _T1& __x, const _T1& __y) const {return __x == __y;}
+    bool operator()(const _T1& __x, const _T2& __y) const {return __x == __y;}
+    bool operator()(const _T2& __x, const _T1& __y) const {return __x == __y;}
+    bool operator()(const _T2& __x, const _T2& __y) const {return __x == __y;}
+};
+
+template <class _T1>
+struct __equal_to<_T1, _T1>
+{
+    bool operator()(const _T1& __x, const _T1& __y) const {return __x == __y;}
+};
+
+template <class _T1>
+struct __equal_to<const _T1, _T1>
+{
+    bool operator()(const _T1& __x, const _T1& __y) const {return __x == __y;}
+};
+
+template <class _T1>
+struct __equal_to<_T1, const _T1>
+{
+    bool operator()(const _T1& __x, const _T1& __y) const {return __x == __y;}
+};
+
+// __less
 
 template <class _T1, class _T2 = _T1>
 struct __less
@@ -32,6 +60,29 @@ struct __less<_T1, const _T1>
 {
     bool operator()(const _T1& __x, const _T1& __y) const {return __x < __y;}
 };
+
+// equal
+
+template <class _InputIterator1, class _InputIterator2, class _BinaryPredicate>
+inline
+bool
+equal(_InputIterator1 __first1, _InputIterator1 __last1, _InputIterator2 __first2, _BinaryPredicate __pred)
+{
+    for (; __first1 != __last1; ++__first1, ++__first2)
+        if (!__pred(*__first1, *__first2))
+            return false;
+    return true;
+}
+
+template <class _InputIterator1, class _InputIterator2>
+inline
+bool
+equal(_InputIterator1 __first1, _InputIterator1 __last1, _InputIterator2 __first2)
+{
+    typedef typename iterator_traits<_InputIterator1>::value_type __v1;
+    typedef typename iterator_traits<_InputIterator2>::value_type __v2;
+    return ft::equal(__first1, __last1, __first2, __equal_to<__v1, __v2>());
+}
 
 // lexicographical_compare
 
