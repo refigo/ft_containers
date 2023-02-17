10a9fee0b382df881878464a71697c4ab59ba035
Refi Go <64242999+refigo@users.noreply.github.com>
2023-02-17 15:47:07 +0900

    feat: Add less in __functional_base.hpp

--- include/__functional_base.hpp
+++ include/__functional_base.hpp
@@ -1,7 +1,9 @@
-#ifndef __MGO__FUNCTIONAL_BASE_
-#define __MGO__FUNCTIONAL_BASE_
+#ifndef __MGO__FUNCTIONAL_BASE_HPP_
+#define __MGO__FUNCTIONAL_BASE_HPP_
 
 namespace ft {
+
+// binary_function
 
 template <class _Arg1, class _Arg2, class _Result>
 struct binary_function
@@ -11,6 +13,15 @@ struct binary_function
 	typedef _Result	result_type;
 };
 
+// less
+
+template <class _Tp>
+struct less : binary_function<_Tp, _Tp, bool>
+{
+	bool operator()(const _Tp& __x, const _Tp& __y) const
+		{return __x < __y;}
+};
+
 } // namespace ft
 
-#endif /* __MGO__FUNCTIONAL_BASE_ */
+#endif /* __MGO__FUNCTIONAL_BASE_HPP_ */
