791fa93a1edab935c6a1dbfd48616e5c05e7c15e
Refi Go <64242999+refigo@users.noreply.github.com>
2023-02-17 15:20:09 +0900

    feat: Add pair and make_pair in pair.hpp

---
+++ include/pair.hpp
@@ -0,0 +1,101 @@
+#ifndef MGO_PAIR_HPP_
+#define MGO_PAIR_HPP_
+
+namespace ft {
+
+template <class _T1, class _T2>
+struct pair
+{
+	typedef _T1 first_type;
+	typedef _T2 second_type;
+
+	_T1	first;
+	_T2	second;
+
+	// default (1)
+	pair() : first(), second() {}
+
+	// copy (2)
+	pair(const _T1& __x, const _T2& __y)
+		: first(__x), second(__y) {}
+
+	// initialization (3)
+	template<class _U1, class _U2>
+	pair(const pair<_U1, _U2>& __p)
+		: first(__p.first), second(__p.second) {}
+
+	// implicitly declared:
+	// copy (1)
+	// pair& operator=(const pair& __other);
+
+	// pair& operator=(const pair& __other)
+	// {
+	// 	if (this == &__other)
+	// 		return (*this);
+	// 	this->first = __other.first;
+	// 	this->second = __other.second;
+	// 	return (*this);
+	// }
+};
+
+// relational operators (pair)
+template <class _T1, class _T2>
+inline
+bool
+operator==(const pair<_T1,_T2>& __x, const pair<_T1,_T2>& __y)
+{
+    return (__x.first == __y.first && __x.second == __y.second);
+}
+
+template <class _T1, class _T2>
+inline
+bool
+operator!=(const pair<_T1,_T2>& __x, const pair<_T1,_T2>& __y)
+{
+    return !(__x == __y);
+}
+
+template <class _T1, class _T2>
+inline
+bool
+operator< (const pair<_T1,_T2>& __x, const pair<_T1,_T2>& __y)
+{
+    return (__x.first < __y.first || (!(__y.first < __x.first) && __x.second < __y.second));
+}
+
+template <class _T1, class _T2>
+inline
+bool
+operator> (const pair<_T1,_T2>& __x, const pair<_T1,_T2>& __y)
+{
+    return (__y < __x);
+}
+
+template <class _T1, class _T2>
+inline
+bool
+operator>=(const pair<_T1,_T2>& __x, const pair<_T1,_T2>& __y)
+{
+    return !(__x < __y);
+}
+
+template <class _T1, class _T2>
+inline
+bool
+operator<=(const pair<_T1,_T2>& __x, const pair<_T1,_T2>& __y)
+{
+    return !(__y < __x);
+}
+
+// make_pair
+template <class _T1, class _T2>
+inline
+pair<_T1,_T2>
+make_pair(_T1 __x, _T2 __y)
+{
+    return (ft::pair<_T1, _T2>(__x, __y));
+}
+
+} // namespace ft
+
+#endif /* MGO_PAIR_HPP_ */
