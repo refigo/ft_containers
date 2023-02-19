025c1aed7bc8328255b972fa34b948741d1114e8
Refi Go <64242999+refigo@users.noreply.github.com>
2023-02-19 13:48:12 +0900

    feat: Set __rb_tree_node class structure

--- include/__rb_tree.hpp
+++ include/__rb_tree.hpp
@@ -3,6 +3,52 @@
 
 namespace ft {
 
+// __rb_tree_node
+
+template <class _Pointer>
+class __rb_tree_end_node
+{
+public:
+    typedef _Pointer pointer;
+    pointer __left_;
+
+    __rb_tree_end_node() : __left_() {}
+};
+
+template <class _VoidPtr>
+class __rb_tree_node_base
+    : public __rb_tree_end_node<_VoidPtr>
+{
+    __rb_tree_node_base(const __rb_tree_node_base&);
+    __rb_tree_node_base& operator=(const __rb_tree_node_base&);
+public:
+    typedef _VoidPtr					pointer;
+    typedef const _VoidPtr				const_pointer;
+    typedef __rb_tree_end_node<pointer> base;
+
+    pointer __right_;
+    pointer __parent_;
+    bool __is_black_;
+
+    __rb_tree_node_base() : __right_(), __parent_(), __is_black_(false) {}
+};
+
+template <class _Tp, class _VoidPtr>
+class __rb_tree_node
+    : public __rb_tree_node_base<_VoidPtr>
+{
+public:
+    typedef __rb_tree_node_base<_VoidPtr> base;
+    typedef _Tp value_type;
+
+    value_type __value_;
+
+    explicit __rb_tree_node(const value_type& __v)
+            : __value_(__v) {}
+};
+
+// __rb_tree
+
 template <class _Tp, class _Compare, class _Allocator>
 class __rb_tree
 {
@@ -17,17 +63,17 @@ public:
 	// iterator
 	typedef	ft::__rb_tree_iterator<value_type>					iterator;
 	typedef	ft::reverse_iterator<iterator>						reverse_iterator;
-
-private:
 	// node
-	typedef	ft::__rb_tree_node<T>								__node;
-	typedef	ft::__rb_tree_node<T>*								__node_pointer;
+	typedef	ft::__rb_tree_node<value_type, 
+						typename allocator_type::void_pointer>	__node;
+	typedef	ft::__rb_tree_node<value_type, 
+						typename allocator_type::void_pointer>*	__node_pointer;
 	typedef	typename allocator_type::template rebind<__node>	__node_allocator; // NOTE
 
 
 
 // public:
-// 	typedef __tree_node<value_type, typename allocator_type::void_pointer> __node;
+// 	typedef __rb_tree_node<value_type, typename allocator_type::void_pointer> __node;
 // 	typedef typename allocator_type::template
 //             rebind_alloc<__node>
 //                                                      __node_allocator;
