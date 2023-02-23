871c72189f6c83b4744ac21764e0f3b27d70e245
Refi Go <64242999+refigo@users.noreply.github.com>
2023-02-23 13:27:59 +0900

    fix: Add const to map.count()

--- include/map.hpp
+++ include/map.hpp
@@ -139,7 +139,7 @@ public:
   iterator find(const key_type& _k)             {return __tree_.find(_k);}
   const_iterator find(const key_type& _k) const {return __tree_.find(_k);}
   // count
-  size_type count(const key_type& _k) { return __tree_.count(_k); }
+  size_type count(const key_type& _k) const { return __tree_.count(_k); }
   // lower_bound
   iterator lower_bound(const key_type& _k) { return __tree_.lower_bound(_k); }
   const_iterator lower_bound(const key_type& _k) const {
