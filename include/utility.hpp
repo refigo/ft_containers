013c2190bb9e130582d18c64097d75ca647a1dd7
refigo <has0521@daum.net>
2023-02-24 18:15:40 +0900

    feat: Add utility.hpp for new tester

---
+++ include/utility.hpp
@@ -0,0 +1,10 @@
+#ifndef MGO_UTILITY_HPP_
+#define MGO_UTILITY_HPP_
+
+#include "algorithm.hpp"
+#include "iterator.hpp"
+#include "pair.hpp"
+#include "type_traits.hpp"
+#include "utils.hpp"
+
+#endif
