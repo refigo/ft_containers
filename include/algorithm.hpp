3982eefc28cde41a1ebabf715a9c9f92a0029c5a
refigo <has0521@daum.net>
2023-02-24 16:47:30 +0900

    refactor: Organize algorithm.hpp

--- include/algorithm.hpp
+++ include/algorithm.hpp
@@ -5,11 +5,10 @@
 
 namespace ft {
 
-// __equal_to
+// equal
 
 template <class _T1, class _T2 = _T1>
-struct __equal_to
-{
+struct __equal_to {
     bool operator()(const _T1& __x, const _T1& __y) const {return __x == __y;}
     bool operator()(const _T1& __x, const _T2& __y) const {return __x == __y;}
     bool operator()(const _T2& __x, const _T1& __y) const {return __x == __y;}
@@ -17,59 +16,25 @@ struct __equal_to
 };
 
 template <class _T1>
-struct __equal_to<_T1, _T1>
-{
+struct __equal_to<_T1, _T1> {
     bool operator()(const _T1& __x, const _T1& __y) const {return __x == __y;}
 };
 
 template <class _T1>
-struct __equal_to<const _T1, _T1>
-{
+struct __equal_to<const _T1, _T1> {
     bool operator()(const _T1& __x, const _T1& __y) const {return __x == __y;}
 };
 
 template <class _T1>
-struct __equal_to<_T1, const _T1>
-{
+struct __equal_to<_T1, const _T1> {
     bool operator()(const _T1& __x, const _T1& __y) const {return __x == __y;}
 };
-
-// __less
-
-template <class _T1, class _T2 = _T1>
-struct __less
-{
-    bool operator()(const _T1& __x, const _T1& __y) const {return __x < __y;}
-    bool operator()(const _T1& __x, const _T2& __y) const {return __x < __y;}
-    bool operator()(const _T2& __x, const _T1& __y) const {return __x < __y;}
-    bool operator()(const _T2& __x, const _T2& __y) const {return __x < __y;}
-};
-
-template <class _T1>
-struct __less<_T1, _T1>
-{
-    bool operator()(const _T1& __x, const _T1& __y) const {return __x < __y;}
-};
-
-template <class _T1>
-struct __less<const _T1, _T1>
-{
-    bool operator()(const _T1& __x, const _T1& __y) const {return __x < __y;}
-};
-
-template <class _T1>
-struct __less<_T1, const _T1>
-{
-    bool operator()(const _T1& __x, const _T1& __y) const {return __x < __y;}
-};
-
-// equal
 
 template <class _InputIterator1, class _InputIterator2, class _BinaryPredicate>
 inline
 bool
-equal(_InputIterator1 __first1, _InputIterator1 __last1, _InputIterator2 __first2, _BinaryPredicate __pred)
-{
+equal(_InputIterator1 __first1, _InputIterator1 __last1, _InputIterator2 __first2, 
+      _BinaryPredicate __pred) {
     for (; __first1 != __last1; ++__first1, ++__first2)
         if (!__pred(*__first1, *__first2))
             return false;
@@ -79,8 +44,7 @@ equal(_InputIterator1 __first1, _InputIterator1 __last1, _InputIterator2 __first
 template <class _InputIterator1, class _InputIterator2>
 inline
 bool
-equal(_InputIterator1 __first1, _InputIterator1 __last1, _InputIterator2 __first2)
-{
+equal(_InputIterator1 __first1, _InputIterator1 __last1, _InputIterator2 __first2) {
     typedef typename iterator_traits<_InputIterator1>::value_type __v1;
     typedef typename iterator_traits<_InputIterator2>::value_type __v2;
     return ft::equal(__first1, __last1, __first2, __equal_to<__v1, __v2>());
@@ -88,11 +52,33 @@ equal(_InputIterator1 __first1, _InputIterator1 __last1, _InputIterator2 __first
 
 // lexicographical_compare
 
+template <class _T1, class _T2 = _T1>
+struct __less {
+    bool operator()(const _T1& __x, const _T1& __y) const {return __x < __y;}
+    bool operator()(const _T1& __x, const _T2& __y) const {return __x < __y;}
+    bool operator()(const _T2& __x, const _T1& __y) const {return __x < __y;}
+    bool operator()(const _T2& __x, const _T2& __y) const {return __x < __y;}
+};
+
+template <class _T1>
+struct __less<_T1, _T1> {
+    bool operator()(const _T1& __x, const _T1& __y) const {return __x < __y;}
+};
+
+template <class _T1>
+struct __less<const _T1, _T1> {
+    bool operator()(const _T1& __x, const _T1& __y) const {return __x < __y;}
+};
+
+template <class _T1>
+struct __less<_T1, const _T1> {
+    bool operator()(const _T1& __x, const _T1& __y) const {return __x < __y;}
+};
+
 template <class _Compare, class _InputIterator1, class _InputIterator2>
 bool
 __lexicographical_compare(_InputIterator1 __first1, _InputIterator1 __last1,
-                          _InputIterator2 __first2, _InputIterator2 __last2, _Compare __comp)
-{
+                          _InputIterator2 __first2, _InputIterator2 __last2, _Compare __comp) {
     for (; __first2 != __last2; ++__first1, ++__first2)
     {
         if (__first1 == __last1 || __comp(*__first1, *__first2))
@@ -107,9 +93,8 @@ template <class _InputIterator1, class _InputIterator2, class _Compare>
 inline
 bool
 lexicographical_compare(_InputIterator1 __first1, _InputIterator1 __last1,
-                        _InputIterator2 __first2, _InputIterator2 __last2, _Compare __comp)
-{
-    typedef typename add_lvalue_reference<_Compare>::type _Comp_ref; // REVIEW: why using?
+                        _InputIterator2 __first2, _InputIterator2 __last2, _Compare __comp) {
+    typedef typename add_lvalue_reference<_Compare>::type _Comp_ref;
     return ft::__lexicographical_compare<_Comp_ref>(__first1, __last1, __first2, __last2, __comp);
 }
 
@@ -117,11 +102,10 @@ template <class _InputIterator1, class _InputIterator2>
 inline
 bool
 lexicographical_compare(_InputIterator1 __first1, _InputIterator1 __last1,
-                        _InputIterator2 __first2, _InputIterator2 __last2)
-{
+                        _InputIterator2 __first2, _InputIterator2 __last2) {
     return ft::lexicographical_compare(__first1, __last1, __first2, __last2,
-                                         __less<typename iterator_traits<_InputIterator1>::value_type,
-                                                typename iterator_traits<_InputIterator2>::value_type>());
+                                       __less<typename iterator_traits<_InputIterator1>::value_type,
+                                              typename iterator_traits<_InputIterator2>::value_type>());
 }
 
 } // namespace ft
