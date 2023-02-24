ff8c4c7307f6dc45248f3130beee6a35acf48d34
refigo <has0521@daum.net>
2023-02-24 18:59:20 +0900

    refactor: Modify Container name in stack for mutant_stack in main_provided.cpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -18,42 +18,42 @@ public:
 	typedef typename container_type::size_type				size_type;
 
 protected:
-	container_type ctnr_;
+	container_type c;
 
 public:
 // (constructor)
 	explicit stack (const container_type& _ctnr = container_type())
-		: ctnr_(_ctnr) {}
+		: c(_ctnr) {}
 
 // operator=
 	stack& operator=(const stack& _other) {
 		if (this != &_other)
-			ctnr_ = _other.ctnr_;
+			c = _other.c;
 		return *this;
 	}
 
 // Element access:
 	// top
-	reference top()							{ return ctnr_.back(); }
-	const_reference top() const	{ return ctnr_.back(); }
+	reference top()							      { return c.back(); }
+	const_reference top() const		    { return c.back(); }
 
 // Capacity:
 	// empty
-	bool empty() const					{ return ctnr_.empty(); }
+	bool empty() const						    { return c.empty(); }
 	// size
-	size_type size() const			{ return ctnr_.size(); }
+	size_type size() const				    { return c.size(); }
 
 // Modifiers:
 	// push
-	void push(const value_type& _v) { ctnr_.push_back(_v); }
+	void push(const value_type& _v) 	{ c.push_back(_v); }
 	// pop
-	void pop() { ctnr_.pop_back(); }
+	void pop() 						        		{ c.pop_back(); }
 
 // Relational operators
 	bool operator==(const stack<_Tp, _Container>& _other) const { 
-		return (ctnr_ == _other.ctnr_); }
+		return (c == _other.c); }
 	bool operator< (const stack<_Tp, _Container>& _other) const {
-		return (ctnr_ < _other.ctnr_); }
+		return (c < _other.c); }
 	bool operator!=(const stack<_Tp, _Container>& _other) const {
 		return !(*this == _other); }
 	bool operator> (const stack<_Tp, _Container>& _other) const {
